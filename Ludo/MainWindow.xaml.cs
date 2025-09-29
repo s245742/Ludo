196359743551b502e3b570614e3bdd2f65722aa2
Rasmus <s245648@student.dtu.dk>
2025-09-29 18:06:34 +0200

    test

--- Ludo/MainWindow.xaml.cs
+++ Ludo/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 
 namespace Ludo
 {
-    /// <summary>
+    /// <summary> 
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window

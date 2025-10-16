2372a2fb46f065c4b3d0bc956f264d644cba9714
Rasmus <s245648@student.dtu.dk>
2025-10-16 09:35:37 +0200

    begynd nav

---
+++ Ludo/Services/GameService.cs
@@ -0,0 +1,75 @@
+﻿using Ludo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.IO;                
+using System.Reflection;
+
+namespace Ludo.Services
+{
+    
+    public class GameService
+    {
+        //String skal tilpasses til den lokale DB
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rasmu\source\repos\Ludo\Ludo\Database\ludo.mdf;Integrated Security = True; Connect Timeout = 30; Encrypt=True";
+
+        public bool add(Game game)
+        {
+            string query = "insert into game values (@Game_Name)";
+            int rowsAffected = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+             {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Game_Name", game.Name);
+                try
+                {
+                    connection.Open();
+                    rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0; //true if updated rows
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+            
+           
+        }
+    
+
+        public ObservableCollection<Game> getAll()
+        {
+            var gamesList = new ObservableCollection<Game>();
+            string query = "SELECT Game_Name FROM Game";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) {
+                        gamesList.Add(new Game
+                        {
+                            Name = reader.GetString(0)
+                        });
+                    }
+                    reader.Close(); 
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return gamesList;
+        }
+    }
+}
---
+++ Ludo/ViewModels/GameViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ludo.Models;
+using Ludo.Services;
+using Ludo.Commands;
+namespace Ludo.ViewModels
+{
+    public class GameViewModel : ViewModelBase
+    {
+
+
+        GameService ObjGameService;
+        public GameViewModel()
+        {
+            ObjGameService = new GameService(); //can now call method
+            LoadData(); //helper method
+            CurrGame = new Game();
+            saveCommand = new RelayCommand(Save);
+        }
+
+        #region Display games
+        private ObservableCollection<Game> gamesList;
+        public ObservableCollection<Game> GamesList
+        {
+            get { return gamesList; }
+            set { gamesList = value; OnPropertyChanged("GamesList"); }
+        }
+
+        //helpermethod display games
+        private void LoadData()
+        {
+            GamesList = ObjGameService.getAll();
+        }
+        #endregion
+
+        #region AddGame
+        private Game currGame;
+
+        public Game CurrGame
+        {
+            get { return currGame; }
+            set { currGame = value; OnPropertyChanged("CurrGame"); }
+        }
+
+
+        #endregion
+
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                currGame.Name = "Martin3"; //change to test
+                var isSaved = ObjGameService.add(currGame);
+                LoadData(); //reload display
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
